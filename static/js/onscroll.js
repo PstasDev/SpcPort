8c5cc98c210fe22b194f3c995d54c0722e6f07ef
Pstas <60182573+PstasDev@users.noreply.github.com>
2024-05-29 22:14:13 +0200

    versenyek aloldal

--- static/js/onscroll.js
+++ static/js/onscroll.js
@@ -1,32 +1,43 @@
 var prevScrollpos = window.pageYOffset;
+
 window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-    } else {
-        document.getElementById("navbar").style.top = "-15rem";
+    var navbar = document.getElementById("navbar");
+    var scrollMoreA = document.getElementById("scrollMoreA");
+    var tothetop = document.getElementById("tothetop");
+
+    if (navbar) {
+        if (prevScrollpos > currentScrollPos) {
+            navbar.style.top = "0";
+        } else {
+            navbar.style.top = "-15rem";
+        }
     }
 
-    if (currentScrollPos < 50) {
-        document.getElementById("scrollMoreA").style.opacity = "1";
-        document.getElementById("scrollMoreA").style.pointerEvents = "auto";
-    } else {
-        document.getElementById("scrollMoreA").style.opacity = "0";
-        document.getElementById("scrollMoreA").style.pointerEvents = "none";
+    if (scrollMoreA) {
+        if (currentScrollPos < 50) {
+            scrollMoreA.style.opacity = "1";
+            scrollMoreA.style.pointerEvents = "auto";
+        } else {
+            scrollMoreA.style.opacity = "0";
+            scrollMoreA.style.pointerEvents = "none";
+        }
     }
 
-    if (currentScrollPos > 500) {
-        document.getElementById("tothetop").style.opacity = "1";
-        document.getElementById("tothetop").style.pointerEvents = "auto";
+    if (tothetop) {
+        if (currentScrollPos > 500) {
+            tothetop.style.opacity = "1";
+            tothetop.style.pointerEvents = "auto";
 
-        document.body.classList.remove("bg-white", "dark:bg-neutral-900");
-        document.body.classList.add("bg-cyan-900", "dark:bg-cyan-600");
-    } else {
-        document.getElementById("tothetop").style.opacity = "0";
-        document.getElementById("tothetop").style.pointerEvents = "none";
+            document.body.classList.remove("bg-white", "dark:bg-neutral-900");
+            document.body.classList.add("bg-cyan-900", "dark:bg-cyan-600");
+        } else {
+            tothetop.style.opacity = "0";
+            tothetop.style.pointerEvents = "none";
 
-        document.body.classList.remove("bg-cyan-900", "dark:bg-cyan-600");
-        document.body.classList.add("bg-white", "dark:bg-neutral-900");
+            document.body.classList.remove("bg-cyan-900", "dark:bg-cyan-600");
+            document.body.classList.add("bg-white", "dark:bg-neutral-900");
+        }
     }
 
     prevScrollpos = currentScrollPos;
